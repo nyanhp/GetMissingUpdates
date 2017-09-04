96933816439ee965847262b38fd4236f14bd4671
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2017-09-04 14:26:26 +0200

    Parameter description added
    plus legal mumbo-jumbo

--- GetMissingUpdates.ps1
+++ GetMissingUpdates.ps1
@@ -10,6 +10,24 @@
     The machine to connect to
 .PARAMETER Path
     The path to the offline scan file
+.PARAMETER DownloadUri
+    THe URI to download wsusscn2.cab from
+.PARAMETER UpdateSearchFilter
+    The actual search filter. Default value 'IsHidden = 0'
+.PARAMETER Credential
+    The credential to use instead of using Kerberos
+.NOTES
+# Disclaimer
+# This module and it's scripts are not supported under any Microsoft standard support program or service.
+# The scripts are provided AS IS without warranty of any kind.
+# Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability
+# or of fitness for a particular purpose.
+# The entire risk arising out of the use or performance of the scripts and documentation remains with you.
+# In no event shall Microsoft, its authors, or anyone else involved in the creation, production,
+# or delivery of the scripts be liable for any damages whatsoever (including, without limitation, damages
+# for loss of business profits, business interruption, loss of business information, or other pecuniary loss)
+# arising out of the use of or inability to use the sample scripts or documentation,
+# even if Microsoft has been advised of the possibility of such damages.
 #>
 param
 (
